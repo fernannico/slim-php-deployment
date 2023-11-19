b9e22c097704fff09e12cf407c1baf09f3c15cb8
fernannico <fernannico95@gmail.com>
2023-11-19 03:48:37 -0300

    validaciones MW extra 1.0.1

--- app/Middlewares/pedidosEstadoMW.php
+++ app/Middlewares/pedidosEstadoMW.php
@@ -5,7 +5,7 @@ use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 use Slim\Psr7\Response;
 require_once './models/Pedido.php';
 
-class pedidosMW
+class pedidosEstadoMW
 {
 
     public function __invoke(Request $request, RequestHandler $handler): Response
